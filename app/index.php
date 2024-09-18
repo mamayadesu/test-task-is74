8e2e91f1ccbea5249934af21b64ed5d535a41980
Semyon Bayandin <mamayadesu@gmail.com>
2024-09-18 15:58:04 +0500

    apache mod rewrite

--- app/index.php
+++ app/index.php
@@ -6,25 +6,6 @@ define("CONFIGS_DIR", ROOT_DIR . "backend/configs/");
 
 require ROOT_DIR . "vendor/autoload.php";
 
-use \test_is74\Controllers\Controller;
-use \test_is74\Database\Database;
+use test_is74\Application;
 
-register_shutdown_function(function() : void {
-    if (Database::isDatabaseInitialized())
-    {
-        Database::getInstance()->close();
-    }
-});
-
-try
-{
-    Controller::runControllerHandler();
-}
-catch (Throwable $e)
-{
-    echo "<pre>";
-    echo "Uncaught " . get_class($e) . " '" . $e->getMessage() . "' in " . $e->getFile() . " on line " . $e->getLine() . "\n\n";
-    echo "Stack Trace:\n";
-    echo $e->getTraceAsString();
-    echo "</pre>";
-}
+Application::run();
---
+++ backend/classes/Application.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace test_is74;
+
+use test_is74\Database\Database;
+use test_is74\Controllers\Controller;
+use Throwable;
+
+class Application
+{
+    public static function run() : void
+    {
+        register_shutdown_function(function() : void {
+            if (Database::isDatabaseInitialized())
+            {
+                Database::getInstance()->close();
+            }
+        });
+
+        try
+        {
+            Controller::runControllerHandler();
+        }
+        catch (Throwable $e)
+        {
+            echo "<pre>";
+            echo "Uncaught " . get_class($e) . " '" . $e->getMessage() . "' in " . $e->getFile() . " on line " . $e->getLine() . "\n\n";
+            echo "Stack Trace:\n";
+            echo $e->getTraceAsString();
+            echo "</pre>";
+        }
+    }
+}
